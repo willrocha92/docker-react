7d45ffbaebf19b34cae0c31a7b2665340cca6afe
root <root@ip-172-31-17-49.us-east-2.compute.internal>
2020-02-19 00:47:37 +0000

    test

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-test('renders lern react link', () => {
+est('renders lern react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
